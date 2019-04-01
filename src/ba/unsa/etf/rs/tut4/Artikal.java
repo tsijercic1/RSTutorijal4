9c06a2209c250eed00c35f64ebc145a199f92e52
tsijercic1 <tsijercic1@etf.unsa.ba>
2019-04-01 10:44:11 +0200

    Napravio klasu Artikal

---
+++ src/ba/unsa/etf/rs/tut4/Artikal.java
@@ -0,0 +1,61 @@
+package ba.unsa.etf.rs.tut4;
+
+public class Artikal {
+    private String sifra;
+    private String naziv;
+    private double cijena;
+
+
+    public Artikal() {
+    }
+
+    public Artikal(String podatak){
+        String[] artikal = podatak.split(",");
+        sifra = artikal[0];
+        naziv = artikal[1];
+        cijena = Double.parseDouble(artikal[2]);
+    }
+
+
+    public String getSifra() {
+        return sifra;
+    }
+
+    public void setSifra(String sifra) {
+        this.sifra = sifra;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public double getCijena() {
+        return cijena;
+    }
+
+    public void setCijena(double cijena) {
+        this.cijena = cijena;
+    }
+
+    @Override
+    public String toString() {
+        String sifra = this.sifra+","+naziv+","+cijena;
+        return sifra;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        Artikal a = (Artikal) o;
+        if (this == o) return true;
+        if(!naziv.equals(a.getNaziv()))return false;
+        if(!sifra.equals(a.getSifra())) return false;
+        if(Double.compare(cijena,a.getCijena())!=0) return false;
+        return true;
+    }
+
+
+}
